c1a5f7ac7a5c0d0d78cac7d92b79642af0a530df
mil-ev <mosbox@bk.ru>
2017-09-28 12:08:31 +0300

    edit clienys carousel

--- js/scripts.js
+++ js/scripts.js
@@ -126,13 +126,13 @@ $(document).ready(function () {
             {
                 breakpoint: 990,
                 settings: {
-                    slidesToShow: 4
+                    slidesToShow: 3
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                     slidesToScroll: 1
                 }
             },
